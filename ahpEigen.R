73078d67cc22c967343781b7267f73e521447463
SawadaSyahmi <farissyahmisamidi@gmail.com>
2021-06-30 22:38:04 +0800

    update on ahp algo
    1. file transfer,
    2. eigen value

--- ahpEigen.R
+++ ahpEigen.R
@@ -1,96 +1,238 @@
 
 library(ahpsurvey)
+library(readxl)
+library(magrittr)
+library(dplyr)
 data(city200)
 
-ahpscale_lvl1 <- city200
-ahpscale_lvl2_1 <- city200
-ahpscale_lvl2_2 <- city200
-ahpscale_lvl2_3 <- city200
-ahpscale_lvl2_4 <- city200
-ahpscale_lvl3_1 <- city200
-ahpscale_lvl3_2 <- city200
-ahpscale_lvl3_3 <- city200
-ahpscale_lvl3_4 <- city200
-ahpscale_lvl3_5 <- city200
-ahpscale_lvl3_6 <- city200
-ahpscale_lvl3_7 <- city200
-ahpscale_lvl3_8 <- city200
-ahpscale_lvl3_9 <- city200
-ahpscale_lvl3_10 <- city200
-ahpscale_lvl3_11 <- city200
-ahpscale_lvl3_12 <- city200
-text()
+
+
+ahpscale_lvl1 <- data.frame(read_excel("SummaryAhpScale.xlsx", sheet = "Level1"))
+ahpscale_lvl2_1 <- data.frame(read_excel("SummaryAhpScale.xlsx", sheet = "Level2_Econ"))
+ahpscale_lvl2_2 <- data.frame(read_excel("SummaryAhpScale.xlsx", sheet = "Level2_Infrastructure"))
+ahpscale_lvl2_3 <- data.frame(read_excel("SummaryAhpScale.xlsx", sheet = "Level2_serviceStandard"))
+ahpscale_lvl2_4 <- data.frame(read_excel("SummaryAhpScale.xlsx", sheet = "Level2_Technical"))
+ahpscale_lvl3_1 <- data.frame(read_excel("SummaryAhpScale.xlsx", sheet = "Level2_Technical"))
+ahpscale_lvl3_2 <- data.frame(read_excel("SummaryAhpScale.xlsx", sheet = "Level2_Technical"))
+ahpscale_lvl3_3 <- data.frame(read_excel("SummaryAhpScale.xlsx", sheet = "Level2_Technical"))
+ahpscale_lvl3_4 <- data.frame(read_excel("SummaryAhpScale.xlsx", sheet = "Level2_Technical"))
+ahpscale_lvl3_5 <- data.frame(read_excel("SummaryAhpScale.xlsx", sheet = "Level2_Technical"))
+ahpscale_lvl3_6 <- data.frame(read_excel("SummaryAhpScale.xlsx", sheet = "Level2_Technical"))
+ahpscale_lvl3_7 <- data.frame(read_excel("SummaryAhpScale.xlsx", sheet = "Level2_Technical"))
+ahpscale_lvl3_8 <- data.frame(read_excel("SummaryAhpScale.xlsx", sheet = "Level2_Technical"))
+ahpscale_lvl3_9 <- data.frame(read_excel("SummaryAhpScale.xlsx", sheet = "Level2_Technical"))
+ahpscale_lvl3_10 <- data.frame(read_excel("SummaryAhpScale.xlsx", sheet = "Level2_Technical"))
+ahpscale_lvl3_11 <- data.frame(read_excel("SummaryAhpScale.xlsx", sheet = "Level2_Technical"))
+ahpscale_lvl3_12 <- data.frame(read_excel("SummaryAhpScale.xlsx", sheet = "Level2_Technical"))
 
 
 
-atts_lvl1 <- c("Technical", "Economical", "Infrastructure", "Service Standard")
-eigenmean_lvl1 <- ahp.aggpref(ahpscale, atts_lvl1, method = "eigen")
-eigen_lvl1
+eigenmean_lvl1 <- ahp(df = ahpscale_lvl1,
+              atts = c("Technical", "Economical", "Infrastructure", "Service Standard"),
+              negconvert = TRUE,
+              reciprocal = TRUE,
+              method = "eigen",
+              aggmethod = "arithmetic",
+              qt = 0.2,
+              censorcr = 0.1,
+              agg = TRUE)
 
-atts_lvl2_1 <- c("Opex", "Capex")
-eigenmean_lvl2_ <- ahp.aggpref(ahpscale_lvl2_1, atts_lvl2_1, method = "eigen")
-eigen_lvl2_
+head(eigenmean_lvl1$indpref)
 
-atts_lvl2_2 <- c("Existing Infrastructure", "Spectrum Availability")
-eigenmean_lvl2_2 <- ahp.aggpref(ahpscale_lvl2_2, atts_lvl2_2, method = "eigen")
-eigen_lvl2_2
+eigenmean_lvl2_1 <- ahp(df = ahpscale_lvl2_1,
+                      atts = c("Opex", "Capex"),
+                      negconvert = TRUE,
+                      reciprocal = TRUE,
+                      method = "eigen",
+                      aggmethod = "arithmetic",
+                      qt = 0.2,
+                      censorcr = 0.1,
+                      agg = TRUE)
 
-atts_lvl2_3 <- c("Installation Complexity", "Maintenance ", "Security")
-eigenmean_lvl2_3 <- ahp.aggpref(ahpscale_lvl2_3, atts_lvl2_3, method = "eigen")
-eigen_lvl2_3
+head(eigenmean_lvl2_1$indpref)
 
-atts_lvl2_4 <- c("NBPLC", "UHF", "Fiber Optics", "Zigbee",  "WIFI/RF Mesh", "4G", "	Private LTE / NB- IoT",  "LoRA", "SATCOM")
-eigenmean_lvl2_4 <- ahp.aggpref(ahpscale_lvl2_4, atts_lvl2_4, method = "eigen")
-eigen_lvl2_4
+eigenmean_lvl2_2 <- ahp(df = ahpscale_lvl2_2,
+                       atts =  c("Existing Infrastructure", "Spectrum Availability"),
+                       negconvert = TRUE,
+                       reciprocal = TRUE,
+                       method = "eigen",
+                       aggmethod = "arithmetic",
+                       qt = 0.2,
+                       censorcr = 0.1,
+                       agg = TRUE)
 
-atts_lvl3_1 <- c("NBPLC", "UHF", "Fiber Optics", "Zigbee",  "WIFI/RF Mesh", "4G", "	Private LTE / NB- IoT",  "LoRA", "SATCOM")
-eigenmean_lvl3_1 <- ahp.aggpref(ahpscale_lvl3_1, atts_lvl3_1, method = "eigen")
-eigen_lvl3_1
+head(eigenmean_lvl2_2$indpref)
 
-atts_lvl3_2 <- c("NBPLC", "UHF", "Fiber Optics", "Zigbee",  "WIFI/RF Mesh", "4G", "	Private LTE / NB- IoT",  "LoRA", "SATCOM")
-eigenmean_lvl3_2 <- ahp.aggpref(ahpscale_lvl3_2, atts_lvl3_2, method = "eigen")
-eigen_lvl3_2
+eigenmean_lvl2_3 <- ahp(df = ahpscale_lvl2_3,
+                        atts =  c("Installation Complexity", "Maintenance ", "Security"),
+                        negconvert = TRUE,
+                        reciprocal = TRUE,
+                        method = "eigen",
+                        aggmethod = "arithmetic",
+                        qt = 0.2,
+                        censorcr = 0.1,
+                        agg = TRUE)
 
-atts_lvl3_3 <- c("NBPLC", "UHF", "Fiber Optics", "Zigbee",  "WIFI/RF Mesh", "4G", "	Private LTE / NB- IoT",  "LoRA", "SATCOM")
-eigenmean_lvl3_3 <- ahp.aggpref(ahpscale_lvl3_3, atts_lvl3_3, method = "eigen")
-eigen_lvl3_3
+head(eigenmean_lvl2_3$indpref)
 
-atts_lvl3_4 <- c("NBPLC", "UHF", "Fiber Optics", "Zigbee",  "WIFI/RF Mesh", "4G", "	Private LTE / NB- IoT",  "LoRA", "SATCOM")
-eigenmean_lvl3_4 <- ahp.aggpref(ahpscale_lvl3_4, atts_lvl3_4, method = "eigen")
-eigen_lvl3_4
 
-atts_lvl3_5 <- c("NBPLC", "UHF", "Fiber Optics", "Zigbee",  "WIFI/RF Mesh", "4G", "	Private LTE / NB- IoT",  "LoRA", "SATCOM")
-eigenmean_lvl3_5 <- ahp.aggpref(ahpscale_lvl3_5, atts_lvl3_5, method = "eigen")
-eigen_lvl3_5
 
-atts_lvl3_6 <- c("NBPLC", "UHF", "Fiber Optics", "Zigbee",  "WIFI/RF Mesh", "4G", "	Private LTE / NB- IoT",  "LoRA", "SATCOM")
-eigenmean_lvl3_6 <- ahp.aggpref(ahpscale_lvl3_6, atts_lvl3_6, method = "eigen")
-eigen_lvl3_6
+eigenmean_lvl2_4 <- ahp(df = ahpscale_lvl2_4,
+                        atts =  c("NBPLC", "UHF", "Fiber Optics", "Zigbee",  "WIFI/RF Mesh", "4G", "	Private LTE / NB- IoT",  "LoRA", "SATCOM"),
+                        negconvert = TRUE,
+                        reciprocal = TRUE,
+                        method = "eigen",
+                        aggmethod = "arithmetic",
+                        qt = 0.2,
+                        censorcr = 0.1,
+                        agg = TRUE)
 
-atts_lvl3_7 <- c("NBPLC", "UHF", "Fiber Optics", "Zigbee",  "WIFI/RF Mesh", "4G", "	Private LTE / NB- IoT",  "LoRA", "SATCOM")
-eigenmean_lvl3_7 <- ahp.aggpref(ahpscale_lvl3_7, atts_lvl3_7, method = "eigen")
-eigen_lvl3_7
+head(eigenmean_lvl2_4$indpref)
 
-atts_lvl3_8 <- c("NBPLC", "UHF", "Fiber Optics", "Zigbee",  "WIFI/RF Mesh", "4G", "	Private LTE / NB- IoT",  "LoRA", "SATCOM")
-eigenmean_lvl3_8 <- ahp.aggpref(ahpscale_lvl3_8, atts_lvl3_8, method = "eigen")
-eigen_lvl3_8
+eigenmean_lvl3_1 <- ahp(df = ahpscale_lvl3_1,
+                        atts =  c("NBPLC", "UHF", "Fiber Optics", "Zigbee",  "WIFI/RF Mesh", "4G", "	Private LTE / NB- IoT",  "LoRA", "SATCOM"),
+                        negconvert = TRUE,
+                        reciprocal = TRUE,
+                        method = "eigen",
+                        aggmethod = "arithmetic",
+                        qt = 0.2,
+                        censorcr = 0.1,
+                        agg = TRUE)
 
-atts_lvl3_9 <- c("NBPLC", "UHF", "Fiber Optics", "Zigbee",  "WIFI/RF Mesh", "4G", "	Private LTE / NB- IoT",  "LoRA", "SATCOM")
-eigenmean_lvl3_9 <- ahp.aggpref(ahpscale_lvl3_9, atts_lvl3_9, method = "eigen")
-eigen_lvl3_9
+head(eigenmean_lvl3_1$indpref)
 
-atts_lvl3_10 <- c("NBPLC", "UHF", "Fiber Optics", "Zigbee",  "WIFI/RF Mesh", "4G", "	Private LTE / NB- IoT",  "LoRA", "SATCOM")
-eigenmean_lvl3_10 <- ahp.aggpref(ahpscale_lvl3_10, atts_lvl3_10, method = "eigen")
-eigen_lvl3_10
 
-atts_lvl3_11 <- c("NBPLC", "UHF", "Fiber Optics", "Zigbee",  "WIFI/RF Mesh", "4G", "	Private LTE / NB- IoT",  "LoRA", "SATCOM")
-eigenmean_lvl3_11 <- ahp.aggpref(ahpscale_lvl3_11, atts_lvl3_11, method = "eigen")
-eigen_lvl3_11
+eigenmean_lvl3_2 <- ahp(df = ahpscale_lvl3_2,
+                        atts =  c("NBPLC", "UHF", "Fiber Optics", "Zigbee",  "WIFI/RF Mesh", "4G", "	Private LTE / NB- IoT",  "LoRA", "SATCOM"),
+                        negconvert = TRUE,
+                        reciprocal = TRUE,
+                        method = "eigen",
+                        aggmethod = "arithmetic",
+                        qt = 0.2,
+                        censorcr = 0.1,
+                        agg = TRUE)
 
-atts_lvl3_12 <- c("NBPLC", "UHF", "Fiber Optics", "Zigbee",  "WIFI/RF Mesh", "4G", "	Private LTE / NB- IoT",  "LoRA", "SATCOM")
-eigenmean_lvl3_12 <- ahp.aggpref(ahpscale_lvl3_12, atts_lvl3_12, method = "eigen")
-eigen_lvl3_12
+head(eigenmean_lvl3_2$indpref)
 
+eigenmean_lvl3_3 <- ahp(df = ahpscale_lvl3_3,
+                        atts =  c("NBPLC", "UHF", "Fiber Optics", "Zigbee",  "WIFI/RF Mesh", "4G", "	Private LTE / NB- IoT",  "LoRA", "SATCOM"),
+                        negconvert = TRUE,
+                        reciprocal = TRUE,
+                        method = "eigen",
+                        aggmethod = "arithmetic",
+                        qt = 0.2,
+                        censorcr = 0.1,
+                        agg = TRUE)
+
+head(eigenmean_lvl3_3$indpref)
+
+eigenmean_lvl3_4 <- ahp(df = ahpscale_lvl3_4,
+                        atts =  c("NBPLC", "UHF", "Fiber Optics", "Zigbee",  "WIFI/RF Mesh", "4G", "	Private LTE / NB- IoT",  "LoRA", "SATCOM"),
+                        negconvert = TRUE,
+                        reciprocal = TRUE,
+                        method = "eigen",
+                        aggmethod = "arithmetic",
+                        qt = 0.2,
+                        censorcr = 0.1,
+                        agg = TRUE)
+
+head(eigenmean_lvl3_4$indpref)
+
+eigenmean_lvl3_5 <- ahp(df = ahpscale_lvl3_5,
+                        atts =  c("NBPLC", "UHF", "Fiber Optics", "Zigbee",  "WIFI/RF Mesh", "4G", "	Private LTE / NB- IoT",  "LoRA", "SATCOM"),
+                        negconvert = TRUE,
+                        reciprocal = TRUE,
+                        method = "eigen",
+                        aggmethod = "arithmetic",
+                        qt = 0.2,
+                        censorcr = 0.1,
+                        agg = TRUE)
+
+head(eigenmean_lvl3_5$indpref)
+
+eigenmean_lvl3_6 <- ahp(df = ahpscale_lvl3_6,
+                        atts =  c("NBPLC", "UHF", "Fiber Optics", "Zigbee",  "WIFI/RF Mesh", "4G", "	Private LTE / NB- IoT",  "LoRA", "SATCOM"),
+                        negconvert = TRUE,
+                        reciprocal = TRUE,
+                        method = "eigen",
+                        aggmethod = "arithmetic",
+                        qt = 0.2,
+                        censorcr = 0.1,
+                        agg = TRUE)
+
+head(eigenmean_lvl3_6$indpref)
+
+eigenmean_lvl3_7 <- ahp(df = ahpscale_lvl3_7,
+                        atts =  c("NBPLC", "UHF", "Fiber Optics", "Zigbee",  "WIFI/RF Mesh", "4G", "	Private LTE / NB- IoT",  "LoRA", "SATCOM"),
+                        negconvert = TRUE,
+                        reciprocal = TRUE,
+                        method = "eigen",
+                        aggmethod = "arithmetic",
+                        qt = 0.2,
+                        censorcr = 0.1,
+                        agg = TRUE)
+
+head(eigenmean_lvl3_7$indpref)
+
+eigenmean_lvl3_8 <- ahp(df = ahpscale_lvl3_8,
+                        atts =  c("NBPLC", "UHF", "Fiber Optics", "Zigbee",  "WIFI/RF Mesh", "4G", "	Private LTE / NB- IoT",  "LoRA", "SATCOM"),
+                        negconvert = TRUE,
+                        reciprocal = TRUE,
+                        method = "eigen",
+                        aggmethod = "arithmetic",
+                        qt = 0.2,
+                        censorcr = 0.1,
+                        agg = TRUE)
+
+head(eigenmean_lvl3_8$indpref)
+
+eigenmean_lvl3_9 <- ahp(df = ahpscale_lvl3_9,
+                        atts =  c("NBPLC", "UHF", "Fiber Optics", "Zigbee",  "WIFI/RF Mesh", "4G", "	Private LTE / NB- IoT",  "LoRA", "SATCOM"),
+                        negconvert = TRUE,
+                        reciprocal = TRUE,
+                        method = "eigen",
+                        aggmethod = "arithmetic",
+                        qt = 0.2,
+                        censorcr = 0.1,
+                        agg = TRUE)
+
+head(eigenmean_lvl3_9$indpref)
+
+eigenmean_lvl3_10 <- ahp(df = ahpscale_lvl3_10,
+                        atts =  c("NBPLC", "UHF", "Fiber Optics", "Zigbee",  "WIFI/RF Mesh", "4G", "	Private LTE / NB- IoT",  "LoRA", "SATCOM"),
+                        negconvert = TRUE,
+                        reciprocal = TRUE,
+                        method = "eigen",
+                        aggmethod = "arithmetic",
+                        qt = 0.2,
+                        censorcr = 0.1,
+                        agg = TRUE)
+
+head(eigenmean_lvl3_10$indpref)
+
+eigenmean_lvl3_11 <- ahp(df = ahpscale_lvl3_11,
+                        atts =  c("NBPLC", "UHF", "Fiber Optics", "Zigbee",  "WIFI/RF Mesh", "4G", "	Private LTE / NB- IoT",  "LoRA", "SATCOM"),
+                        negconvert = TRUE,
+                        reciprocal = TRUE,
+                        method = "eigen",
+                        aggmethod = "arithmetic",
+                        qt = 0.2,
+                        censorcr = 0.1,
+                        agg = TRUE)
+
+head(eigenmean_lvl3_11$indpref)
+
+eigenmean_lvl3_12 <- ahp(df = ahpscale_lvl3_12,
+                        atts =  c("NBPLC", "UHF", "Fiber Optics", "Zigbee",  "WIFI/RF Mesh", "4G", "	Private LTE / NB- IoT",  "LoRA", "SATCOM"),
+                        negconvert = TRUE,
+                        reciprocal = TRUE,
+                        method = "eigen",
+                        aggmethod = "arithmetic",
+                        qt = 0.2,
+                        censorcr = 0.1,
+                        agg = TRUE)
+
+head(eigenmean_lvl3_12$indpref)
 
 
 
